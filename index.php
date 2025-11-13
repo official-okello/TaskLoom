e92750ed586547b2fad56abe8eff0cd23c2ffb94
Julius Okello <officialokello@gmail.com>
2025-11-13 15:01:45 +0300

    Changed the app title, removed margins on header

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyToDo - Task Management</title>
+    <title>TaskLoom</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/filters.css">
     <script>
